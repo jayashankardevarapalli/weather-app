645894c04e6348b1c05ebee915d6ec56ed8cedc8
s3r1alsh0ck <mrj@shell.localdomain>
2022-07-28 21:50:27 +0530

    hr line

--- sections/About.js
+++ sections/About.js
@@ -27,7 +27,7 @@ const About = () => {
             <div className="flex flex-col sm:flex-row mt-10">
               <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
                 <div className="w-200 h-200 rounded-full inline-flex items-center justify-center">
-                  <Image src={mrj} width="400" height="600" />
+                  <Image src={mrj} width="280" height="400" />
                 </div>
                 <div className="flex flex-col items-center text-center justify-center">
                   <h2 className="font-medium title-font mt-4 text-lg">
@@ -87,7 +87,7 @@ const About = () => {
                   </div>
                 </div>
               </div>
-              <div className="sm:w-2/3 sm:pl-8 sm:py-6 sm:border-l border-gray-200 sm:border-t-0 border-t mt-2 pt-12 align-center sm:mt-0 text-center sm:text-left">
+              <div className="sm:w-2/3 sm:pl-8 sm:py-6 mt-2 pt-12 align-center sm:mt-0 text-center sm:text-left">
                 <p className="leading-relaxed text-lg mb-3 mt-24">
                   I am D.Jaya Shankar Kumar, I am currently pursuing my MCA Post
                   Graduartion degree from Gayatri Vidya Parishad College of
